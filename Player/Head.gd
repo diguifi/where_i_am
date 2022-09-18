389035b2df200716b313bdb125b8a8bed8f6a202
diguifi <lumia-diguifi@hotmail.com>
2022-09-18 16:01:50 -0300

    Fix fog

--- Player/Head.gd
+++ Player/Head.gd
@@ -2,22 +2,25 @@ extends Spatial
 
 export(NodePath) var cam_path := NodePath("Camera")
 onready var cam: Camera = get_node(cam_path)
+onready var fog = $Fog
 
 export var mouse_sensitivity := 2.0
 export var y_limit := 90.0
 var mouse_axis := Vector2()
 var rot := Vector3()
 
-func _ready() -> void:
+func _ready():
+	if get_node('/root/Game/Lighting/WorldEnvironment').get_environment().fog_enabled:
+		fog.visible = true
 	mouse_sensitivity = mouse_sensitivity / 1000
 	y_limit = deg2rad(y_limit)
 
-func _input(event: InputEvent) -> void:
+func _input(event: InputEvent):
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		mouse_axis = event.relative
 		camera_rotation()
 
-func camera_rotation() -> void:
+func camera_rotation():
 	rot.y -= mouse_axis.x * mouse_sensitivity
 	rot.x = clamp(rot.x - mouse_axis.y * mouse_sensitivity, -y_limit, y_limit)
 	
