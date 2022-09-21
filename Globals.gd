b95d958dd5466425b089df025f06436c5bf259b6
diguifi <lumia-diguifi@hotmail.com>
2022-09-21 17:28:52 -0300

    Fix global state reset

--- Globals.gd
+++ Globals.gd
@@ -7,12 +7,21 @@ var pieces = 0
 var total_pieces = 5
 var world_saturation = 1
 var env_node
+var initial_saturation = 1
+var initial_fog = 1
+var initial_blur = 1
+var has_set_initials = false
 
 func _ready():
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear2db(0.1))
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sfx"), linear2db(0.5))
 	
 func _physics_process(delta):
+	if end == 2 and !has_set_initials:
+		has_set_initials = true
+		initial_saturation = env_node.adjustment_saturation
+		initial_fog = env_node.fog_depth_end
+		initial_blur = env_node.dof_blur_far_distance
 	if end == 2 and world_saturation < 1.8:
 		world_saturation += delta
 		env_node.adjustment_saturation = world_saturation
@@ -24,3 +33,12 @@ func get_piece():
 	if pieces >= total_pieces:
 		end = 2
 		env_node = get_node('/root/Game/Lighting/WorldEnvironment').get_environment()
+
+func reset():
+	end = 1
+	pieces = 0
+	total_pieces = 5
+	world_saturation = 1
+	env_node.adjustment_saturation = initial_saturation
+	env_node.fog_depth_end = initial_fog
+	env_node.dof_blur_far_distance = initial_blur
--- MagicDoor/Portal.gd
+++ MagicDoor/Portal.gd
@@ -9,4 +9,5 @@ func _final_place_reached():
 func _on_Area_body_entered(body):
 	if body.is_in_group("player"):
 		TriggerManager.reset()
+		Globals.reset()
 		get_tree().reload_current_scene()
